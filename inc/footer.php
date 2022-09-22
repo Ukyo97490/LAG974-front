96cfdbb37bd509eb249c1a6b48a7e31036a9309d
Ukyo974 <damour.gregory@gmail.com>
2022-09-22 18:53:50 +0400

    Modif du Footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,2 +1,5 @@
-test
+ <!-- footer section  -->
+ <footer>
+        <span>Created By <a href="https://github.com/Ukyo97490">Grégory DAMOUR </a> | <span class="far fa-copyright"></span> 2022 All rights reserved.</span>
+</footer>
 <script src="app.js"></script>
